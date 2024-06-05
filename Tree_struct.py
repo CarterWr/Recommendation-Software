89d4474837c5cd857fd22ac9b1df67ee58f7b080
Carter Wright <skycreed04@gmail.com>
2024-06-05 16:59:46 -0400

    Added Tree Node class for tree struct

--- Tree_struct.py
+++ Tree_struct.py
@@ -1,1 +1,8 @@
 # File For Basic Tree Data Struct
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
