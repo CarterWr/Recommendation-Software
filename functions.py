4e542b6149a89fa0c622d354c8e1b43e2cb93f3b
Carter Wright <skycreed04@gmail.com>
2024-06-05 15:34:42 -0400

    added start and end functions. Added main function to main.py

--- functions.py
+++ functions.py
@@ -1,1 +1,23 @@
 # where BFS algo goes + other functions used in this program
+def greet():
+    """greet message to user"""
+    pass
+
+def user_input():
+    """Gathers user input for the autocomplete()"""
+    return input("Type the first few letters of the genre you would like to be reccomended to you: ")
+
+
+
+def goodbye():
+    """goodbye message to user"""
+    pass
+
+
+def reccomendation_main():
+    greet()
+
+    user_input()
+
+    goodbye()
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from functions import reccomendation_main
+
 # where executable code runs
 if __name__ == "__main__":
-    pass
+    reccomendation_main()
