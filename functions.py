d4266db44ed1ed25172c0684fe35352480701832
Carter Wright <skycreed04@gmail.com>
2024-06-05 16:15:38 -0400

    Added working autoComplete function and pattern search

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 # where BFS algo goes + other functions used in this program
+from data import types
 def greet():
     """greet message to user"""
     pass
@@ -6,6 +7,37 @@ def greet():
 def user_input():
     """Gathers user input for the autocomplete()"""
     return input("Type the first few letters of the genre you would like to be reccomended to you: ")
+
+def pattern_search(user_input, target):
+    if user_input in target:
+        return True
+    return False
+
+def auto_complete():
+    """Uses input function to get user input and checks if the begging part of genre equals input
+    
+       return: output(string): the result of matchs of user input
+    """
+    user = user_input()
+    output = []
+    
+    for genre in types:
+        if pattern_search(user, genre):
+            output.append(genre)
+    
+    if len(output) == 0:
+        print("\nLooks like we don't have what you're looking for please try a diffrent genre.\n")
+        auto_complete()
+
+    if len(output) > 1:
+        print("\nLooks like you have a few options, please select one of the options given:\n {}\n".format(output))
+        auto_complete()
+    else:
+        return output[0]
+
+
+
+    
 
 
 
@@ -15,9 +47,10 @@ def goodbye():
 
 
 def reccomendation_main():
+    """This function acts as the main function to call all needed functions"""
     greet()
 
-    user_input()
+    auto_complete()
 
     goodbye()
 
